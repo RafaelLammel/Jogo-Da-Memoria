e074d7e8a67c7c193a53cfd24faf66f51acddf87
Rafael Lammel Marinheiro <rafaellmarinheiro42@gmail.com>
2019-06-14 21:59:14 -0300

    Base do jogo: cartas no tabuleiro e click para revelar

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public GameObject cardBack;
+    private int id;
+    private void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+    public void changeSprite(int id, Sprite img)
+    {
+        this.id = id;
+        GetComponent<SpriteRenderer>().sprite = img;
+    }
+}
---
+++ Assets/Scripts/GameContoller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameContoller : MonoBehaviour
+{
+    public GameObject original;
+    public Sprite[] imgs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] ids = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        ids = ShuffleArray(ids);
+        original.GetComponent<Card>().changeSprite(ids[0], imgs[ids[0]]);
+        for (int i = 1; i < 4; i++)
+        {
+            GameObject novaCarta = Instantiate(original);
+            novaCarta.GetComponent<Card>().changeSprite(ids[i], imgs[ids[i]]);
+            novaCarta.transform.position = new Vector3(original.transform.position.x+(i*3),original.transform.position.y,original.transform.position.z);
+        }
+        for(int i = 0; i < 4; i++)
+        {
+            GameObject novaCarta = Instantiate(original);
+            novaCarta.GetComponent<Card>().changeSprite(ids[i+4], imgs[ids[i+4]]);
+            novaCarta.transform.position = new Vector3(original.transform.position.x + (i * 3), original.transform.position.y+6, original.transform.position.z);
+        }
+    }
+
+    private int[] ShuffleArray(int[] ids)
+    {
+        int[] shuffled = ids;
+        for (int i = 0; i < shuffled.Length; i++)
+        { 
+            int tmp = shuffled[i];
+            int r = Random.Range(i, shuffled.Length);
+            shuffled[i] = shuffled[r];
+            shuffled[r] = tmp;
+        }
+        return shuffled;
+    }
+}
